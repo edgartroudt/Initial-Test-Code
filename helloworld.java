7d23676f5de38a0cd63d4da831973ea7dc1311d7
edgartroudt <42075091+edgartroudt@users.noreply.github.com>
2023-08-28 13:42:14 +0000

    Changed the print statement at the end with name.
    Continuing to make this program strong.

--- helloworld.java
+++ helloworld.java
@@ -2,13 +2,13 @@ import java.util.*;
 
 public class helloworld {
 
-	public static void main ( String[] args ) {
-		System.out.println("Hello, world!");
-		System.out.println("What's your name?");
-		Scanner s = new Scanner ( System.in );
-		String myName = s.nextLine();
-		System.out.println("Your name is " + myName);
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+    System.out.println("What's your name?");
+    Scanner s = new Scanner(System.in);
+    String myName = s.nextLine();
+    System.out.println("Well, hello " + myName + " good to meet you.");
 
-	}
+  }
 
 }
